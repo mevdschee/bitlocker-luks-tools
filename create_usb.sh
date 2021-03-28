c587c370e26290369d723478bb0b2c185a590242
Maurits van der Schee <maurits@vdschee.nl>
2021-03-28 16:04:35 +0200

    Create create_usb.sh

---
+++ create_usb.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# change sdX to the right device
+echo "start=2048 size=32768 type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B bootable attrs=RequiredPartition" | sudo sfdisk --wipe always /dev/sdX -X gpt
+sudo mkfs.fat /dev/sdX1
+sudo mount /dev/sda1 /mnt
+sudo cp keys/*.BEK /mnt
+# sudo fatattr +sh /mnt/*
+sudo umount /mnt
