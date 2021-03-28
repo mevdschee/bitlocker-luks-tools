1dffce719461fe8c1663ccd2d31051c5254c9aa9
Maurits van der Schee <maurits@vdschee.nl>
2021-03-28 16:54:32 +0200

    modified:   create_usb.sh

--- create_usb.sh
+++ create_usb.sh
@@ -3,6 +3,6 @@
 echo "start=2048 size=32768 type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B bootable attrs=RequiredPartition" | sudo sfdisk --wipe always /dev/sdX -X gpt
 sudo mkfs.fat /dev/sdX1
 sudo mount /dev/sda1 /mnt
-sudo cp keys/*.BEK /mnt
+sudo find keys -type f -name "*.BEK" -exec cp {} /mnt \;
 # sudo fatattr +sh /mnt/*
 sudo umount /mnt
