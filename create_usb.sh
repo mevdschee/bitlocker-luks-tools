38715b9dac35db6c9b36adb31f90297edde3486f
Maurits van der Schee <maurits@vdschee.nl>
2021-03-29 01:06:43 +0200

    select usb device

--- create_usb.sh
+++ create_usb.sh
@@ -1,28 +1,31 @@
 # !/bin/bash
-# change sdX to the right device
-echo Which removable USB drives do you want to ERASE?
+echoerr() { cat <<< "$@" 1>&2; }
 devs=$(lsblk --noheadings --nodeps --output NAME,SIZE,TRAN,RM | sed 's/\s\s*/|/g' | grep "usb|1$")
 if [ -z $devs ]; then
-echo no removable USB drives found
-exit
+echoerr no removable USB drives found
+exit 1
 fi
+echo Which removable USB drives do you want to ERASE?
 select dev in $devs
 do
 dev=$(echo $dev | cut -d'|' -f1)
 spec="start=2048 size=32768 type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B bootable attrs=RequiredPartition" 
-echo $spec | sudo sfdisk -q --wipe always /dev/${dev} -X gpt
+echo $spec | sfdisk -q --wipe always /dev/${dev} -X gpt
 if [ $? -ne 0 ]; then
-echo fdisk failed
-exit
+echoerr sfdisk failed, needs sudo?
+exit 2
 fi
-sudo mkfs.fat /dev/${dev}1
-sudo mount /dev/${dev}1 /mnt 
+mkfs.fat /dev/${dev}1
 if [ $? -ne 0 ]; then
-echo mount failed
-exit
+echoerr mkfs failed
+exit 3
 fi
-sudo find keys -type f -name "*.BEK" -exec cp -v {} /mnt \;
-# sudo fatattr +sh /mnt/*
-sudo umount /mnt
-exit
+mount /dev/${dev}1 /mnt 
+if [ $? -ne 0 ]; then
+echoerr mount failed
+exit 4
+fi
+find keys -type f -name "*.BEK" -exec cp -v {} /mnt \;
+umount /mnt
+exit 0
 done
